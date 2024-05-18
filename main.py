af10053e4aeab0ec835d6e778a331376f064b916
bonitoflakes101 <154349729+bonitoflakes101@users.noreply.github.com>
2024-05-18 23:26:04 +0800

    WITH VOICE main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from google.cloud import texttospeech
 import os
+from playsound import playsound
 
 # replace directory with your own
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\xiang\Downloads\prac-423707-4b3eb9019a6f.json"
 client = texttospeech.TextToSpeechClient()
 
+# put output text from AI here
+text_to_speak = "THANK GOD IT'S WORKING"
+input_text = texttospeech.SynthesisInput(text=text_to_speak)
 
-input_text = texttospeech.SynthesisInput(text="AGASDASD")
-
-# Note: the voice can also be specified by name.
-# Names of voices can be retrieved with client.list_voices().
 voice = texttospeech.VoiceSelectionParams(
     language_code="en-US",
     name="en-US-Studio-O",
 )
 
+# use MP3 encoding since we are saving as MP3
 audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.LINEAR16,
+    audio_encoding=texttospeech.AudioEncoding.MP3,
     speaking_rate=1
 )
 
@@ -25,6 +26,10 @@ response = client.synthesize_speech(
 )
 
 # The response's audio_content is binary.
-with open("output.mp3", "wb") as out:
+output_path = r"C:\Users\xiang\VSCProjects\TTS_googlecloud\output.mp3"
+with open(output_path, "wb") as out:
     out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    print(f'Audio content written to file "{output_path}"')
+
+# plays the mp3 file 
+playsound(output_path)
